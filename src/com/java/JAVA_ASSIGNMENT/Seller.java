cc56430b9f385e7d3b96a3869ec39cacbc17baad
Harrydai0216 <60716542+Harrydai0216@users.noreply.github.com>
2021-12-05 02:35:33 -0500

    Update Seller.java
    Add some variables in Seller to accomplish future goals

--- src/com/java/JAVA_ASSIGNMENT/Seller.java
+++ src/com/java/JAVA_ASSIGNMENT/Seller.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 
 public class Seller  extends User{
 
-    private double sellerProfit;
-    private String sellerUsername;
-
+   
 
     // Add the attributes found in the page below to this class
     //https://help.shopee.com.my/my/s/article/How-do-I-register-as-a-seller
+
+
+    public double sellerProfit;
+    public String sellerUsername;
+    public String sellerEmail;
+    public String sellerPhonenumber;
+    public String sellerbankaccount;
+    public String selleraddress;
+    public String sellerpassword;
 
 
     public double getSellerProfit() {
@@ -26,5 +33,45 @@ public class Seller  extends User{
 
     public void setSellerUsername(String sellerUsername) {
         this.sellerUsername = sellerUsername;
+    }
+
+    public String getSellerEmail() {
+        return sellerEmail;
+    }
+
+    public void setSellerEmail(String sellerEmail) {
+        this.sellerEmail = sellerEmail;
+    }
+
+    public String getSellerPhonenumber() {
+        return sellerPhonenumber;
+    }
+
+    public void setSellerPhonenumber(String sellerPhonenumber) {
+        this.sellerPhonenumber = sellerPhonenumber;
+    }
+
+    public String getSellerbankaccount() {
+        return sellerbankaccount;
+    }
+
+    public void setSellerbankaccount(String sellerbankaccount) {
+        this.sellerbankaccount = sellerbankaccount;
+    }
+
+    public String getSelleraddress() {
+        return selleraddress;
+    }
+
+    public void setSelleraddress(String selleraddress) {
+        this.selleraddress = selleraddress;
+    }
+
+    public String getSellerpassword() {
+        return sellerpassword;
+    }
+
+    public void setSellerpassword(String sellerpassword) {
+        this.sellerpassword = sellerpassword;
     }
 }
