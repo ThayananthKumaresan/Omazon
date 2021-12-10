504b0ce588f74e12f6e7234dc9226d9cd0e124a0
Harrydai0216 <60716542+Harrydai0216@users.noreply.github.com>
2021-12-10 16:35:06 -0500

    Update FeedbackDaoImp.java

--- src/com/java/JAVA_ASSIGNMENT/FeedbackDaoImp.java
+++ src/com/java/JAVA_ASSIGNMENT/FeedbackDaoImp.java
@@ -7,22 +7,44 @@ public class FeedbackDaoImp implements FeedbackDao{
 
 
     @Override
-    public Feedback getFeedback(String feedbackID) {
+    public Feedback getFeedback(String productID) {
+
+        for (int j = 0; j < feedbackDataBase.size(); j++) {
+
+            if (feedbackDataBase.get(j).getFeedbackProduct().getProductID().equals(productID)) {
+                return feedbackDataBase.get(j);
+            }
+
+        }
         return null;
     }
 
     @Override
     public void updateFeedback(Feedback feedback) {
-
+        for (int j = 0; j < userFeedbackDatabase.size(); j++) {
+            if (feedback.getUserUsername().equals(userFeedbackDatabase.get(j).getUserUsername())) {
+                serFeedbackDatabase.set(j,user);
+            }
+        }
     }
 
-    @Override
-    public void deleteFeedback(Feedback feedback) {
-
+    public boolean deleteFeedback(Feedback feedback) {
+        for (int i = 0; i <  userFeedbackDatabase.size(); i++) {
+            if(feedback.getUserUsername().equals(userFeedbackDatabase.get(j).getUserUsername())) {
+                userFeedbackDatabase[i] = userFeedbackDatabase[size- 1];
+                userFeedbackDatabase[size - 1] = null;
+                size--;
+                return true;
+            }
+        }
+        return false;
     }
-
-    @Override
-    public void addFeedback(Feedback feedback) {
-
+    public boolean addFeedback(Feedback feedback){
+        for(int i = 0; i<feedbackDataBase.size() ; i++){
+            feedbackDataBase[size] = feedback;
+            size++;
+            return true;
+        }
+        return false;
     }
 }
